e1f6ac833ae5e99a2f995111fac5e60c27fc8623
kanghana1 <qazplm12093@naver.com>
2024-08-13 00:36:36 +0900

    chore: project setting

---
+++ src/test/java/com/server/demo/growup/GrowupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.demo.growup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
